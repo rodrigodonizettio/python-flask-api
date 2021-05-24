4bdf081bfe250280e32eb1f0b1b5709cddbd759e
Rodrigo Donizetti de Oliveira <rodrigodonizettio@gmail.com>
2021-05-24 08:27:45 -0300

    Adding a getAll endpoint to retrieve a fake DB data and convert it to JSON object

--- api.py
+++ api.py
@@ -1,11 +1,31 @@
 import flask
+from flask import request, jsonify
 
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+
+fakeDbData = [
+  {
+    'id': 1,
+    'code': 1312,
+    'name': 'Rodrigo Donizetti'
+  },
+  {
+    'id': 2,
+    'code': 1313,
+    'name': 'Rodrigo Vimieiro'
+  },
+]
+
 @app.route('/', methods=['GET'])
 def home():
-  return "<h1>Hello Flask API!</h1>"
+  return "<h1>Hello Flask API!</h1><h3>To retrieve DB Data try /api/v1/employee/all </h3>"
+
+@app.route('/api/v1/employee/all', methods=['GET'])
+def employee_all():
+  return jsonify(fakeDbData)
+
 
 app.run()
